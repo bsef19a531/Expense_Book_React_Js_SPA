e32397abb99dd269ad6306907031f0a77c09988f
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-07-22 19:05:51 +0500

    Added Dynamic Expense output logic

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -13,10 +13,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={FilterChangeHandler} />
-
-      <ExpenseItem title={props.expense[0].title} amount={props.expense[0].amount} date={props.expense[0].date} />
-      <ExpenseItem title={props.expense[1].title} amount={props.expense[1].amount} date={props.expense[1].date} />
-      <ExpenseItem title={props.expense[2].title} amount={props.expense[2].amount} date={props.expense[2].date} />
+      {props.expense.map((exp) => (<ExpenseItem title={exp.title} amount={exp.amount} date={exp.date} />))}
     </Card>
   )
 }
