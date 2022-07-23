9c96b4910e790857a5ec802cf2722e332affe765
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-07-23 13:56:23 +0500

    Restructured project directories

--- components/ExpenseAmount/ExpenseAmount.js
+++
@@ -1,9 +0,0 @@
-import "./ExpenseAmount.css";
-
-const ExpenseAmount = (props) => {
-    return (
-        <div className="expense-amount">Rs: {props.amount}</div>
-    )
-}
-
-export default ExpenseAmount;
